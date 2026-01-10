182be27d0a204a41ccc3b9f5f099789b22fba066
Apll1211 <z490135438@gmail.com>
2026-01-10 10:11:31 +0800

    网站构建测试

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 強制將 better-sqlite3 視為外部依賴，不進行打包
+  serverExternalPackages: ["better-sqlite3"],
+
+  // 允许的外部域名
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "apll.qzz.io",
+        port: "3000",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
